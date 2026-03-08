fbc26978472f198d81a82cfb399a748ab19b35af
Michalina Nestorowicz <misia.nestorowicz@gmail.com>
2026-03-08 17:41:49 +0100

    Update settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,3 +35,33 @@ vim.lsp.config("ruff", {
 
 vim.lsp.enable 'pyright'
 vim.lsp.enable 'ruff'
+
+
+-- oil
+require("oil").setup({
+  columns = {
+    "permissions",
+    "size",
+    "mtime",
+  },
+  keymaps = {
+    ["g?"] = { "actions.show_help", mode = "n" },
+    ["<CR>"] = "actions.select",
+    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+    ["<C-t>"] = { "actions.select", opts = { tab = true } },
+    ["<C-p>"] = "actions.preview",
+    ["<C-c>"] = { "actions.close", mode = "n" },
+    ["<C-l>"] = "actions.refresh",
+    ["`"] = { "actions.parent", mode = "n" },
+    ["_"] = { "actions.open_cwd", mode = "n" },
+    ["<leader>cc"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+    ["gs"] = { "actions.change_sort", mode = "n" },
+    ["gx"] = "actions.open_external",
+    ["g."] = { "actions.toggle_hidden", mode = "n" },
+    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+  },
+  view_options = {
+    show_hidden = true,
+  }
+})
